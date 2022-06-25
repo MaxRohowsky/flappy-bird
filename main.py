ed4817e9ac9a9f55e2b0c4f9879207d2397ef165
max <68344959+codewmax@users.noreply.github.com>
2022-06-26 00:15:40 +0200

    Initial Commit 2

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import pygame
-import os
 import random
+
 pygame.init()
+clock = pygame.time.Clock()
 
-# Global Constants
-win_height =720
-win_width =551
+# Window
+win_height = 720
+win_width = 551
 window = pygame.display.set_mode((win_width, win_height))
-
 
 # Images
 bird_images = [pygame.image.load("assets/bird-down.png"),
-        pygame.image.load("assets/bird-mid.png"),
-        pygame.image.load("assets/bird-up.png")]
+               pygame.image.load("assets/bird-mid.png"),
+               pygame.image.load("assets/bird-up.png")]
 skyline_image = pygame.image.load("assets/background.png")
 ground_image = pygame.image.load("assets/ground.png")
 bottom_pipe_image = pygame.image.load("assets/pipe.png")
 top_pipe_image = pygame.transform.rotate(bottom_pipe_image, 180)
-
-
 
 
 class Pipe:
@@ -27,30 +25,25 @@ class Pipe:
         self.bottom_pipe = bottom_pipe_image
         self.top_pipe = top_pipe_image
         # Position
-        self.x = 500
-        self.top_pipe_y = random.randint(-600, -480)
+        self.top_pipe_x, self.bottom_pipe_x = 700, 700
         self.delta = random.randint(90, 130)
         self.length = bottom_pipe_image.get_height()
+        self.top_pipe_y = random.randint(-600, -480)
         self.bottom_pipe_y = self.top_pipe_y + self.length + self.delta
 
     def update(self):
-        self.x -= 1
+        self.top_pipe_x -= 1
+        self.bottom_pipe_x -= 1
 
     def draw(self, win):
-        win.blit(self.top_pipe, (self.x, self.top_pipe_y))
-        win.blit(self.bottom_pipe, (self.x, self.bottom_pipe_y))
+        win.blit(self.top_pipe, (self.top_pipe_x, self.top_pipe_y))
+        win.blit(self.bottom_pipe, (self.bottom_pipe_x, self.bottom_pipe_y))
 
 
 
-
-
-
-
-clock = pygame.time.Clock()
 pipes = []
-x=0
+x = 0
 counter = 5
-
 
 run = True
 while run:
@@ -60,12 +53,9 @@ while run:
             run = False
     window.fill((0, 0, 0))
 
-
     # Skyline Background
-    window.blit(skyline_image, (0,0))
+    window.blit(skyline_image, (0, 0))
     userInput = pygame.key.get_pressed()
-
-
 
     for pipe in pipes:
         pipe.draw(window)
@@ -79,15 +69,10 @@ while run:
         x = 0
     x -= 1
 
-
     if counter <= 0:
         pipes.append(Pipe())
         counter = random.randint(180, 300)
     counter -= 1
 
-
-
-
-
-    clock.tick(100)
+    clock.tick(60)
     pygame.display.update()
