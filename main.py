f2f7909be4512e986971c9132a4fadd9bc620044
max <68344959+codewmax@users.noreply.github.com>
2022-06-26 02:15:18 +0200

    Initial Commit 2

--- main.py
+++ main.py
@@ -15,64 +15,75 @@ bird_images = [pygame.image.load("assets/bird-down.png"),
                pygame.image.load("assets/bird-up.png")]
 skyline_image = pygame.image.load("assets/background.png")
 ground_image = pygame.image.load("assets/ground.png")
-bottom_pipe_image = pygame.image.load("assets/pipe.png")
-top_pipe_image = pygame.transform.rotate(bottom_pipe_image, 180)
+top_pipe_image = pygame.image.load("assets/pipe_top.png")
+bottom_pipe_image = pygame.image.load("assets/pipe_bottom.png")
 
 
-class Pipe:
-    def __init__(self):
-        # Images
-        self.bottom_pipe = bottom_pipe_image
-        self.top_pipe = top_pipe_image
-        # Position
-        self.top_pipe_x, self.bottom_pipe_x = 700, 700
-        self.delta = random.randint(90, 130)
-        self.length = bottom_pipe_image.get_height()
-        self.top_pipe_y = random.randint(-600, -480)
-        self.bottom_pipe_y = self.top_pipe_y + self.length + self.delta
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y, image):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
 
     def update(self):
-        self.top_pipe_x -= 1
-        self.bottom_pipe_x -= 1
-
-    def draw(self, win):
-        win.blit(self.top_pipe, (self.top_pipe_x, self.top_pipe_y))
-        win.blit(self.bottom_pipe, (self.bottom_pipe_x, self.bottom_pipe_y))
+        self.rect.x -= scroll_speed
+        if self.rect.x <= -win_width:
+            self.kill()
 
 
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = ground_image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
-pipes = []
-x = 0
-counter = 5
+    def update(self):
+        self.rect.x -= scroll_speed
+        if self.rect.x <= -win_width:
+            self.kill()
+
+
+pipes = pygame.sprite.Group()
+ground = pygame.sprite.Group()
+
+x_pos_ground = 0
+pipe_timer = 0
+scroll_speed = 1
+ground.add(Ground(x_pos_ground, 520), Ground(win_width + x_pos_ground, 520))
+
 
 run = True
 while run:
-    # Exit
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     window.fill((0, 0, 0))
 
-    # Skyline Background
     window.blit(skyline_image, (0, 0))
+
     userInput = pygame.key.get_pressed()
 
-    for pipe in pipes:
-        pipe.draw(window)
-        pipe.update()
+    pipes.update()
+    pipes.draw(window)
 
-    # Ground
-    window.blit(ground_image, (x, 520))
-    window.blit(ground_image, (win_width + x, 520))
-    if x == -win_width:
-        window.blit(ground_image, (win_width + x, 0))
-        x = 0
-    x -= 1
+    ground.update()
+    ground.draw(window)
 
-    if counter <= 0:
-        pipes.append(Pipe())
-        counter = random.randint(180, 300)
-    counter -= 1
+    if len(ground) == 1:
+        ground.add(Ground(win_width, 520))
+
+    if pipe_timer <= 0:
+        x_top, x_bottom = 700, 700
+        y_top = random.randint(-600, -480)
+        y_bottom = y_top + random.randint(90, 130) + bottom_pipe_image.get_height()
+        pipes.add(Pipe(x_top, y_top, top_pipe_image))
+        pipes.add(Pipe(x_bottom, y_bottom, bottom_pipe_image))
+        pipe_timer = random.randint(180, 300)
+    pipe_timer -= 1
 
     clock.tick(60)
     pygame.display.update()
